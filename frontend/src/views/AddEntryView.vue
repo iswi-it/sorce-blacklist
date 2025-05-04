60949fb74afd0d474477123e253e74bdd4e96000
Niklas Loos <niklas.loos@tu-ilmenau.de>
2025-05-05 00:10:24 +0200

    improve design of add entry form

--- frontend/src/views/AddEntryView.vue
+++ frontend/src/views/AddEntryView.vue
@@ -6,6 +6,15 @@
     <BAlert v-model="error_countdown" variant="danger" dismissible fade>
       Entry was successfully added to SOrCE-Blacklist!
     </BAlert>
+    <h3>Add Participant to Blacklist</h3>
+    <p>
+      This form will let you add a participant to the SOrCE-Blacklist. All data
+      you enter will be first hashed locally and the send to the server to be
+      stored in a central database. Please make sure to follow the instructions
+      of each form entry, so entries can be matched later. Hashed Data will be
+      normalized, so the blacklist and matching are insensitive towards capital
+      letters.
+    </p>
     <form @submit.prevent="submit">
       <div class="mb-3">
         <label for="name" class="form-label">Name:</label>
@@ -16,6 +25,10 @@
           class="form-control"
           required="true"
         />
+        <div class="form-text">
+          Full name of the participant that should be put on the blacklist. Data
+          will be hashed!
+        </div>
       </div>
       <div class="mb-3">
         <label for="email" class="form-label">E-Mail:</label>
@@ -26,6 +39,10 @@
           class="form-control"
           required="true"
         />
+        <div class="form-text">
+          E-Mail of the participant that should be put on the blacklist. Data
+          will be hashed!
+        </div>
       </div>
       <div class="mb-3">
         <label for="email" class="form-label">Nationality:</label>
@@ -36,6 +53,16 @@
           class="form-control"
           required="true"
         />
+        <div class="form-text">
+          Please use the full english name of the participants nationality, e.g
+          as written in
+          <a
+            href="https://github.com/Imagin-io/country-nationality-list/blob/master/countries.csv"
+            target="_blank"
+            >this list</a
+          >
+          in the column nationality.
+        </div>
       </div>
       <div class="mb-3">
         <label for="birtdate" class="form-label">Birthdate:</label>
@@ -46,18 +73,29 @@
           model-type="dd/MM/yyyy"
           format="dd/MM/yyyy"
         />
+        <div class="form-text">
+          Please choose the birthdate of participant that will be put on the
+          blacklist. Data will be hashed!
+        </div>
       </div>
       <div class="mb-3">
         <label for="comment" class="form-label">Comment:</label>
-        <input
+        <textarea
           type="textarea"
           name="comment"
           v-model="form.comment"
           class="form-control"
           required="true"
         />
+        <div class="form-text">
+          Field for a general comment, e.g. why the person was put on the
+          blacklist. Don't put privat data like names or emails in this field as
+          it won't be hashed!
+        </div>
       </div>
-      <button type="submit" class="btn btn-primary">Submit</button>
+      <button type="submit" class="btn btn-primary">
+        Blacklist Participant
+      </button>
     </form>
   </section>
 </template>
